1666611e46c982e870cdd16e3fc808ff12bbe430
Douglas Alves <douglas.expalves@gmail.com>
2017-09-15 16:06:30 -0300

    user model

---
+++ models/users.js
@@ -0,0 +1,12 @@
+'use strict'
+
+let db = require('../libs/db')
+
+module.exports = db.Model.extend({
+  tableName: 'users',
+  uuid: true,
+  hasTimestamps : true,
+  bcrypt: { field : 'password'},
+  hidden : ['password'] 
+
+})
