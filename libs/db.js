01deaf1f9ce0a2aed1164d1374ef69e428c521fb
Douglas Alves <douglas.expalves@gmail.com>
2017-09-15 16:05:11 -0300

    uuid

---
+++ libs/db.js
@@ -0,0 +1,10 @@
+'use strict'
+let knexfile = require('../knexfile')
+let knex = require('knex')(knexfile[process.env.NODE_ENV || 'development'])
+let bookshelf = require('bookshelf')(knex)
+let uuid = require('bookshelf-uuid')
+let bcrypt = require('bookshelf-bcrypt')
+bookshelf.plugin(bcrypt)
+bookshelf.plugin('visibility')
+
+module.exports = bookshelf
