988250392c22f43702e72ec202e7e33599e32b3e
Douglas Alves <douglas.expalves@gmail.com>
2017-09-15 08:53:28 -0300

    estruturação

---
+++ index.js
@@ -0,0 +1,57 @@
+'use strict'
+
+let Hapi = require('hapi')
+let Good = require('good')
+
+let app = new Hapi.Server()
+
+app.connection({
+  port: '3000',
+  compression: true
+})
+
+app.register(
+  {
+    register: require('hapi-router'),
+    options: {
+      routes: 'app/**/routes.js'
+    }
+})
+
+
+app.register(
+  {
+    register: Good,
+    options: {
+      reporters: {
+        console: [
+          {
+            module: 'good-squeeze',
+            name: 'Squeeze',
+            args: [
+              {
+                response: '*',
+                log: '*'
+              }
+            ]
+          },
+          {
+            module: 'good-console'
+          },
+          'stdout'
+        ]
+      }
+    }
+  },
+  err => {
+    if (err) {
+      throw err // something bad happened loading the plugin
+    }
+
+    app.start(err => {
+      if (err) throw err
+
+      app.log('Servidor rodando em: ', app.info.uri)
+    })
+  }
+)
