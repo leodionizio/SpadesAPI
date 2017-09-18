2e3df1c6655828fa526bb38c149b842ff7cf3cb3
Douglas Alves <douglas.expalves@gmail.com>
2017-09-18 00:49:44 -0300

    model updated

--- migrations/20170915150307_posts.js
+++ migrations/20170915150307_posts.js
@@ -1,12 +1,12 @@
 'use strict'
 
 exports.up = knex =>
-knex.schema.createTable('users', table => {
+knex.schema.createTable('posts', table => {
   table.uuid('id').primary()
-  table.string('title', 70).notNullable()
+  table.string('title').notNullable()
   table.string('content').notNullable()
   table.string('tags')
-  table.integer('id_user').notNullable().references('id').inTable('users')
+  // table.uuid('id').references('users.id')
   table.timestamps()
 })
 
