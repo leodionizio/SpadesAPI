09347e7fb4ba2b06b502ec3719b5a092b4ee1c95
Douglas Alves <douglas.expalves@gmail.com>
2017-10-01 20:57:55 -0300

    corrected session.login replys

--- src/sessions/handler.js
+++ src/sessions/handler.js
@@ -6,7 +6,6 @@ let User = require('../../models/users')
 const tokenKey = '123456789'
 exports.login = (request, reply) => {
   let session
-
   User.forge(request.payload.user)
     .fetch({ require: true })
     .then(result => {
@@ -14,7 +13,15 @@ exports.login = (request, reply) => {
       return result.compare(request.payload.password)
     })
     .then(isValid => {
-      if (isValid) {
+      if (!isValid) {
+        return reply({
+          error: true,
+          status: 401,
+          message: 'Sessão não autorizada'
+        })
+          .code(401)
+          .send()
+      } else {
         Jwt.sign(
           {
             id: session.id,
@@ -26,14 +33,22 @@ exports.login = (request, reply) => {
             expiresIn: '1h'
           },
           (err, token) => {
-            if (err) return Boom.unauthorized(err)
-    
-            token = 'Bearer '+ token          
-            
+            if (err) {
+              reply({
+                error: true,
+                status: 202,
+                err: err,
+                message: 'Sessão não autorizada'
+              })
+                .code(401)
+                .send()
+            }
+
+            token = 'Bearer ' + token
+
             return reply({
               error: false,
-              status: 202,
-              message: 'Session authorized'
+              status: 202
             })
               .header('Authorization', token)
               .code(202)
@@ -42,7 +57,14 @@ exports.login = (request, reply) => {
         )
       }
     })
-    .catch(err => 
-      reply(Boom.unauthorized('Sua sessão não pode ser autorizada!')))
-      .code(401)
+    .catch(err =>
+      reply({
+        error: true,
+        status: 401,
+        err: err,
+        message: 'Sessão não autorizada'
+      })
+        .code(401)
+        .send()
+    )
 }
