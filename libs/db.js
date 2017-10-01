98ac856006f627cf43d8929572f2d8c6a19d5f3b
Douglas Alves <douglas.expalves@gmail.com>
2017-10-01 20:56:00 -0300

    took uuid out

--- libs/db.js
+++ libs/db.js
@@ -3,10 +3,8 @@
 let knexfile = require('../knexfile')
 let knex = require('knex')(knexfile[process.env.NODE_ENV || 'development'])
 let bookshelf = require('bookshelf')(knex)
-let uuid = require('bookshelf-uuid')
 let bcrypt = require('bookshelf-bcrypt')
 
-bookshelf.plugin(uuid)
 bookshelf.plugin(bcrypt)
 bookshelf.plugin('visibility')
 
